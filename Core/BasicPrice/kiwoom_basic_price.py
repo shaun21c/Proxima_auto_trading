f5fc2802b0f4d498a508d89df313fe41ce5903c9
shaun21c <shaun21c@naver.com>
2024-12-23 17:29:55 +0900

    함수 작성 초안안

--- Core/BasicPrice/kiwoom_basic_price.py
+++ Core/BasicPrice/kiwoom_basic_price.py
@@ -1,39 +1,33 @@
+# CommRqData("RQName", "TRCode", 0, "ScreenNo")
+# RQName: 사용자 구분명으로 사용자가 직접 정의하는 요청의 이름
+# TRCode: 요청하는 TR의 코드
+# 0: 조회(0), 연속(2)
+# ScreenNo: 화면번호
 
 class KiwoomBasicPrice:
+    def __init__(self, kiwoom):
+        self.kiwoom = kiwoom
+        self.screen_no_counter = 1000
+    
+    def get_screen_no(self):
+        self.screen_no_counter += 1
+        return str(self.screen_no_counter)
 
+    def request_opt10001_stock_basic_info(self, code):
+        """
+            주식기본정보요청하는 메서드
+            input: code(str) -> output: None
+        """
+        screen_no = self.get_screen_no()
+        self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+        self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "주식기본정보요청", "opt10001", 0, screen_no)
 
-    def request_opt10001_basic_info(self, code):
-        # Set input values and request opt10001 data
-        self.set_input_value("종목코드", code)
-        self.comm_rq_data("opt10001_req", "opt10001", 0, "0101")
+    def request_opt10003_stock_trade_info(self, code):
+        """
+            주식 체결정보요청하는 메서드
+            input: code(str) -> output: None
+        """
+        screen_no = self.get_screen_no()
+        self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+        self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "체결정보요청", "opt10003", 0, screen_no)
 
-    def request_opt10002_detail_info(self, code):
-        # Set input values and request opt10002 data
-        self.set_input_value("종목코드", code)
-        self.comm_rq_data("opt10002_req", "opt10002", 0, "0102")
-
-    def request_opt10003_price_history(self, code):
-        # Set input values and request opt10003 data
-        self.set_input_value("종목코드", code)
-        self.comm_rq_data("opt10003_req", "opt10003", 0, "0103")
-
-    def request_opt10004_volume_info(self, code):
-        # Set input values and request opt10004 data
-        self.set_input_value("종목코드", code)
-        self.comm_rq_data("opt10004_req", "opt10004", 0, "0104")
-
-    def request_opt10005_account_status(self):
-        # Request opt10005 data
-        self.comm_rq_data("opt10005_req", "opt10005", 0, "0105")
-
-    def request_opt10006_buy_sell_info(self, code):
-        # Set input values and request opt10006 data
-        self.set_input_value("종목코드", code)
-        self.comm_rq_data("opt10006_req", "opt10006", 0, "0106")
-
-    def request_opt10007_other_data(self, code):
-        # Set input values and request opt10007 data
-        self.set_input_value("종목코드", code)
-        self.comm_rq_data("opt10007_req", "opt10007", 0, "0107")
-
-# ...existing code...
